b4adc2f056e6e79482f99fd60456295d2422c06c
Antti Kangas <antti.kangas99@gmail.com>
2020-06-28 19:45:28 +0300

    Add basic world generation

---
+++ Controllers/World.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Stores data for the current world.
+/// </summary>
+public class World : MonoBehaviour
+{
+    [Header("World Parameters")]
+    [SerializeField] int width = 10;
+    [SerializeField] int height = 10;
+
+    public static int Width => instance.width;
+    public static int Height => instance.height;
+
+    static World instance;
+
+    private void Start()
+    {
+        WorldGraphics.RefreshWorld();
+    }
+
+    private void Awake()
+    {
+        instance = this;
+    }
+}
---
+++ Controllers/WorldGraphics.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Responsible for the graphical representation of the world.
+/// </summary>
+public class WorldGraphics : MonoBehaviour
+{
+
+    [SerializeField] GameObject tileObject = default;
+    static GameObject[,] tiles;
+
+    static WorldGraphics instance;
+
+    /// <summary>
+    /// Refresh the world graphics according to the current state of the game.
+    /// </summary>
+    public static void RefreshWorld()
+    {
+        int width = World.Width;
+        int height = World.Height;
+
+        tiles = new GameObject[width, height];
+
+        for(int x = 0; x < width; x++)
+        {
+            for(int y = 0; y < height; y++)
+            {
+                if(tiles[x, y] == null)
+                    tiles[x, y] = Instantiate(instance.tileObject, new Vector3(x, y, 0), Quaternion.identity, instance.transform);
+            }
+        }
+    }
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+}
