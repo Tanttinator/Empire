4a0e095631118bc339efdeb4db4170fc0a768053
Antti Kangas <antti.kangas99@gmail.com>
2020-06-30 00:14:24 +0300

    Add Coords and Direction

---
+++ Data Structures/Coords.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Data structure to represent points in a 2D grid.
+/// </summary>
+public struct Coords
+{
+    public int x { get; private set; }
+    public int y { get; private set; }
+
+    public Coords(int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+
+    /// <summary>
+    /// Returns the coordinates one step along the given direction.
+    /// </summary>
+    /// <param name="dir"></param>
+    /// <returns></returns>
+    public Coords Neighbor(Direction dir) => this + dir.offset;
+
+    public static implicit operator Coords(Vector2 v2)
+    {
+        return new Coords((int)v2.x, (int)v2.y);
+    }
+
+    public static implicit operator Vector2(Coords c)
+    {
+        return new Vector2(c.x, c.y);
+    }
+
+    public static Coords operator +(Coords a, Coords b)
+    {
+        return new Coords(a.x + b.x, a.y + b.y);
+    }
+
+    public static Coords operator -(Coords a, Coords b)
+    {
+        return new Coords(a.x - b.x, a.y - b.y);
+    }
+
+    public static bool operator ==(Coords a, Coords b)
+    {
+        return a.x == b.x && a.y == b.y;
+    }
+
+    public static bool operator !=(Coords a, Coords b)
+    {
+        return a.x != b.x || a.y != b.y;
+    }
+
+    public override bool Equals(object obj)
+    {
+        if (obj is Coords c) return c == this;
+        return false;
+    }
+
+    public override int GetHashCode()
+    {
+        return base.GetHashCode();
+    }
+
+    public override string ToString()
+    {
+        return "(" + x + ", " + y + ")";
+    }
+}
---
+++ Data Structures/Direction.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+/// <summary>
+/// Data structure to represent direction in a 2D grid.
+/// </summary>
+public class Direction
+{
+    public Coords offset { get; protected set; }
+
+    int id = -1;
+    int ID
+    {
+        get
+        {
+            if(id == -1)
+            {
+                id = Array.IndexOf(directions, this);
+            }
+            return id;
+        }
+    }
+
+    public Direction Clockwise => directions[(ID + 1) % directions.Length];
+    public Direction CounterClockwise => directions[(ID - 1 + directions.Length) % directions.Length];
+    public Direction Opposite => directions[(ID + directions.Length / 2) % directions.Length];
+
+    public Direction(Coords offset)
+    {
+        this.offset = offset;
+    }
+
+    public static Direction NORTH = new Direction(new Coords(0, 1));
+    public static Direction NORTH_EAST = new Direction(new Coords(1, 1));
+    public static Direction EAST = new Direction(new Coords(1, 0));
+    public static Direction SOUTH_EAST = new Direction(new Coords(1, -1));
+    public static Direction SOUTH = new Direction(new Coords(0, -1));
+    public static Direction SOUTH_WEST = new Direction(new Coords(-1, -1));
+    public static Direction WEST = new Direction(new Coords(-1, 0));
+    public static Direction NORTH_WEST = new Direction(new Coords(-1, 1));
+
+    public static Direction[] directions = new Direction[]
+    {
+        NORTH, NORTH_EAST, EAST, SOUTH_EAST, SOUTH, SOUTH_WEST, WEST, NORTH_WEST
+    };
+}
